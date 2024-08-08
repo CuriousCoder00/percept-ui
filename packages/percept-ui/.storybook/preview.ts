5a020c94d1ae94555ba784e04496463274ffe6d9
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-09 00:44:12 +0530

    chore: Add Storybook configuration files

---
+++ packages/percept-ui/.storybook/preview.ts
@@ -0,0 +1,17 @@
+import type { Preview } from "@storybook/react";
+
+import "../lib/tailwind.css";
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  tags: ["autodocs"],
+};
+
+export default preview;
