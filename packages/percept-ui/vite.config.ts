acf928331cef80ae30cec3c77287874262750425
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-09 00:43:59 +0530

    chore: Update Vite configuration to include tsconfigPaths plugin and build options

--- packages/percept-ui/vite.config.ts
+++ packages/percept-ui/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { resolve } from "path"; // Import the resolve function from the path module
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: resolve("components/main.ts"), 
+      name: "percept-ui",
+      fileName: "percept-ui",
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "react/jsx-runtime": "react/jsx-runtime",
+        },
+      },
+    },
+  },
+});
