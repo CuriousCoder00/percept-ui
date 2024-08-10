1cc05e59cc6260f147d0c7c436b02a7aa277242d
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-10 09:27:01 +0530

    "Updated Storybook config, package-lock, and dependencies; refactored App.tsx and component imports; modified tailwind.config.js, tsconfig.app.json, and vite.config.ts"

--- packages/percept-ui/components/main.ts
+++
@@ -1,6 +0,0 @@
-import "./tailwind.css";
-
-import { Button } from "./Button/Button";
-import { Header } from "./Header/Header";
-import { DropDown } from "./Dropdown/DropDown";
-export { Button, Header, DropDown };
