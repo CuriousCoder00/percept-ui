cfe7a67397b67ffabc9b96b3cb3e4c9d1f8f14b1
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-10 09:28:53 +0530

    "Updated App.tsx imports and modified Header component usage; changed vite.config.ts entry point from components/main.ts to src/main.ts"

--- packages/percept-ui/tailwind.config.js
+++ packages/percept-ui/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: ["./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
