ccf24bbd4da22fe57c762924c5f036f99ea9acbf
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-11 08:51:47 +0530

    refactor: Update tailwind.config.js

--- package/tailwind.config.js
+++ package/tailwind.config.js
@@ -1,21 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
 import colors from "tailwindcss/colors";
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        primary: colors.blue,
-        accent: colors.purple,
-        danger: colors.red,
-        success: colors.green,
-        warning: colors.yellow,
-        info: colors.cyan,
-        light: colors.gray,
-        dark: colors.slate,
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
