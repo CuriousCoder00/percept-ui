badc053d86f81eebf61c4c2da61355a1b61fcab3
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-12 20:19:05 +0530

    chore:  Input component storybook

--- package/src/components/Input/index.stories.tsx
+++ package/src/components/Input/index.stories.tsx
@@ -1,11 +1,28 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { Input } from ".";
 
+const Variants = ["classic", "standard", "ghost"];
+const Radiuses = ["none", "sm", "md", "lg", "xl", "full"];
+
 const meta: Meta<typeof Input> = {
   title: "Components/Input",
   component: Input,
   parameters: {
     layout: "centered",
+  },
+  argTypes: {
+    variant: {
+      options: Variants,
+      control: { type: "select" },
+    },
+    // size: {
+    //   options: Sizes,
+    //   control: { type: "select" },
+    // },
+    radius: {
+      options: Radiuses,
+      control: { type: "select" },
+    },
   },
 };
 
@@ -13,10 +30,23 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const InputComponent: Story = {
+export const Classic: Story = {
   args: {
     placeholder: "Input",
-    colorscheme: 'blue',
+    variant: "classic",
   },
 };
 
+export const Standard: Story = {
+  args: {
+    variant: "standard",
+    placeholder: "Input",
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    variant: "ghost",
+    placeholder: "Input",
+  },
+};
