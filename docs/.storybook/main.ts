b4deaf93debf4e9c4c58cb6e44e4cdfb40b54f9e
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-21 02:32:25 +0530

    chore: Resolved build error for Storybook staticDirs path to public url

--- docs/.storybook/main.ts
+++ docs/.storybook/main.ts
@@ -13,7 +13,7 @@ const config: StorybookConfig = {
     name: "@storybook/nextjs",
     options: {},
   },
-  staticDirs: ["..\\public"],
+  staticDirs: ["../public"],
   docs: {
     autodocs: "tag",
   },
