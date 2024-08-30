0edf78a9347dc5567510a78f26d1c8b3958868f2
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-31 01:49:06 +0530

    feat: Add PulseLoader component for displaying a pulsating loader

---
+++ package/src/components/Loader/PulseLoader/index.tsx
@@ -0,0 +1,9 @@
+import { cn } from "../../../utils";
+import { VariantProps } from "class-variance-authority";
+import { pulseLoaderStyles } from "./PulseColorSchemes";
+
+type pulseLoaderProps = VariantProps<typeof pulseLoaderStyles>;
+
+export const PulseLoader: React.FC<pulseLoaderProps> = ({ color, size }) => (
+  <div className={cn(pulseLoaderStyles({ size, color }))} />
+);
