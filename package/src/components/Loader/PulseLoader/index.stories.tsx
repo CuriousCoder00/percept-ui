4f353249671799d0fc2d0ef9d1d849ea47ab35b2
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-31 01:49:15 +0530

    feat: Add PulseLoader component stories with default variants and options

---
+++ package/src/components/Loader/PulseLoader/index.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { PulseLoader } from ".";
+
+const Sizes = [1, 2, 3, 4, 5];
+const Colors = [
+  "dark",
+  "blue",
+  "red",
+  "green",
+  "yellow",
+  "cyan",
+  "gray",
+  "emerald",
+  "rose",
+  "amber",
+  "orange",
+  "pink",
+  "purple",
+  "indigo",
+  "teal",
+  "lime",
+  "sky",
+  "black",
+  "light",
+];
+
+const meta: Meta<typeof PulseLoader> = {
+  title: "Components/Loaders/PulseLoader",
+  component: PulseLoader,
+  parameters: {
+    layout: "centered",
+  },
+  argTypes: {
+    color: {
+      options: Colors,
+      control: { type: "select" },
+    },
+    size: {
+      options: Sizes,
+      control: { type: "select" },
+    }
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const PulsePingLoader: Story = {
+  args: {
+    size: 3,
+    color: "blue",
+  },
+};
