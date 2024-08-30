e5f05d60ef6efb24b943454e11cc1a01f7979644
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-31 01:48:50 +0530

    feat: Add Spinner component for displaying a loading spinner

---
+++ package/src/components/Loader/Spinner/index.tsx
@@ -0,0 +1,9 @@
+import { cn } from "../../../utils";
+import { VariantProps } from "class-variance-authority";
+import { spinnerStyles } from "./SpinnerColorSchemes";
+
+type spinnerProps = VariantProps<typeof spinnerStyles>;
+
+export const Spinner: React.FC<spinnerProps> = ({ color, size, variant }) => (
+  <div className={cn(spinnerStyles({ size, color, variant }))} />
+);
