96fc09aaa6eec030825a77dd004b0cf1d858d8e2
Kapil Jangid <kapiljangid040@gmail.com>
2024-08-31 21:48:04 +0530

    feat: Add Avatar, Badge, Spinner, and PulseLoader components

--- package/src/components/index.ts
+++ package/src/components/index.ts
@@ -1,3 +1,7 @@
+export { Avatar } from "./Avatar";
+export { Badge } from "./Badge";
 export { Button } from "./Button";
 export { Input } from "./Input";
-export { Badge } from "./Badge";
+export { Spinner } from "./Loader/Spinner";
+export { PulseLoader } from "./Loader/PulseLoader";
+export { SquareRotate } from "./Loader/SqaureRotate";
