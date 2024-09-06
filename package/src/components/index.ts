86d8dbf0ff376c46a3cf1d7d7eb018f988aa78a6
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-06 09:05:36 +0530

    feat: Add exports for AlertDialogBody, AlertDialogTrigger, Accordion, Background, and update Alert Dialog export

--- package/src/components/index.ts
+++ package/src/components/index.ts
@@ -1,10 +1,16 @@
 export { Avatar } from "./Avatar";
 export { Alert } from "./Alert"; 
-export { AlertDialog } from "./Alert Dialog";
+export { AlertDialog, AlertDialogBody, AlertDialogTrigger  } from "./Alert Dialog";
+export { Accordion } from "./Accordion"
+
 export { Badge } from "./Badge";
 export { Button } from "./Button";
+export { Background } from "./Backgrounds/Grid_DotBackground"
+
 export { Input } from "./Input";
+
 export { PulseLoader } from "./Loader/PulseLoader";
+
 export { Separator } from "./Separator";
 export { Spinner } from "./Loader/Spinner";
 export { SquareRotate } from "./Loader/SqaureRotate";
