188e69991a975d484c7a13ecf61285f38597bbd2
Rishang <rishangkumar9693@gmail.com>
2024-09-12 20:53:43 +0530

    exported the multisearch comp

--- package/src/components/index.ts
+++ package/src/components/index.ts
@@ -1,6 +1,7 @@
 export { Avatar } from "./Avatar";
 export { Alert } from "./Alert"; 
-export {AutoCompleteSearch} from "./Input-search/AutoComplete-Input"
+export {AutoCompleteSearch} from "./Input-search/AutoComplete-Input"; 
+export {MultiSearchAutocomplete} from "./Input-search/Multisearch"
 export { AlertDialog, AlertDialogBody, AlertDialogTrigger  } from "./Alert Dialog";
 export { Accordion } from "./Accordion"
 export { Badge } from "./Badge";
