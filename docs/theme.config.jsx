e616a828d791105c24e367eb2787d3710ead23ea
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-12 22:46:32 +0530

    Refactor project structure and configuration files

---
+++ docs/theme.config.jsx
@@ -0,0 +1,9 @@
+export default {
+    logo: <span>Percept UI</span>,
+    docsRepositoryBase: 'https://github.com/curiouscoder00/percept-ui',
+    project: {
+        link: 'https://github.com/curiouscoder00/percept-ui',
+        
+    },
+    // ... other theme options
+}
