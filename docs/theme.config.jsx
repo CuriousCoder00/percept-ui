a84b8fb72abbe30097a5199bf814a644487770d1
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-14 01:40:00 +0530

    Refactor theme.config.jsx to remove unused import

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,5 +1,5 @@
 import Footer from "./components/Footer";
-import "./app/globals.css"
+
 export default {
     logo: <span>Percept UI</span>,
     docsRepositoryBase: 'https://github.com/CuriousCoder00/percept-ui/tree/main/docs',
