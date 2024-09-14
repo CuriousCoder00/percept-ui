f99f94aaf265eb0e832d9b80a9eaa159c8ece616
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-14 21:07:00 +0530

    Refactor .gitignore to remove unnecessary code

---
+++ docs/app/layout.jsx
@@ -0,0 +1,31 @@
+import localFont from "next/font/local";
+import "../styles/globals.css";
+import { ThemeProvider } from 'next-themes'
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata = {
+  title: "Percept UI",
+  description: "An open-source component library optimized for fast development, easy maintenance, and accessibility.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
