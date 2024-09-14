5114ca1fd5283077853b5aff49d8d8c5aab06341
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-14 10:29:25 +0530

    Refactor Logo component to update SVG dimensions and remove unused code

---
+++ docs/components/Logo.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <svg width="60" height="60" viewBox="0 0 55 85" fill="none" xmlns="http://www.w3.org/2000/svg">
+            {/* <rect width="88" height="72" fill="#1E1E1E" /> */}
+            {/* <path d="M3.36562 69.75L44 2.88823L84.6344 69.75H3.36562Z" stroke="black" stroke-width="3" /> */}
+            <ellipse cx="44" cy="47.5" rx="20" ry="19.5" fill="#00B5F7" />
+            <circle cx="44" cy="48" r="13" fill="#050D10" />
+            <circle cx="44" cy="48" r="13" fill="#050D10" />
+            <circle cx="44" cy="48" r="13" fill="#050D10" />
+            <circle cx="44" cy="48" r="13" fill="#050D10" />
+            <ellipse cx="55.5" cy="47.5" rx="5.5" ry="6.5" fill="white" fill-opacity="0.38" />
+            <ellipse cx="56.5" cy="52.5" rx="3.5" ry="4.5" fill="white" fill-opacity="0.79" />
+        </svg>
+    )
+}
+
+export default Logo
