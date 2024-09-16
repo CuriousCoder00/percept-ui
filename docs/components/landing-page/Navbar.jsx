5b82b980ba795233092be151aaf9efdc753dd744
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-17 00:19:11 +0530

    Refactor Navbar component to update layout and navigation links

--- docs/components/landing-page/Navbar.jsx
+++ docs/components/landing-page/Navbar.jsx
@@ -5,12 +5,12 @@ import Link from 'next/link'
 import { BiLogoGithub } from "react-icons/bi"
 const Navbar = () => {
     return (
-        <header className='flex justify-between items-center font-[family-name:var(--font-geist-sans)] text-white px-10 border-b-2 border-b-slate-800'>
+        <header className='flex justify-between items-center font-[family-name:var(--font-geist-sans)] text-white px-10 border-b-2 border-b-slate-800 overflow-hidden'>
             <Link href={"/"} className="flex items-center justify-center gap-2">
                 <Logo />
                 <span className="my-0 font-bold text-2xl">Percept UI</span>
             </Link>
-            <nav className='flex justify-between items-center gap-3'>
+            <nav className='flex justify-between items-center gap-8'>
                 <a href='/docs/getting-started' className='text-[var(--geist-foreground)]'>Docs</a>
                 <a href='https://github.com/curiouscoder00/percept-ui' className='text-[var(--geist-foreground)]'>
                     <BiLogoGithub className='text-xl' />
