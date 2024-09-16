a4f0e9776fc4bb5e8587a87d0c971dd5881f0096
Kapil Jangid <kapiljangid040@gmail.com>
2024-09-17 00:07:47 +0530

    Refactor Announcement component to update background color and badge color

--- docs/components/landing-page/Announcement.jsx
+++ docs/components/landing-page/Announcement.jsx
@@ -3,11 +3,11 @@ import { Badge } from '..'
 
 const Announcement = () => {
     return (
-        <div className='flex w-full bg-slate-950 text-white p-1'>
+        <div className='flex w-full bg-blue-700 text-white p-2'>
             <a className="flex w-full justify-center items-center gap-4" href="https://docs-percept-ui.vercel.app/docs/getting-started">
                 🎉 Percept UI is released. Read more →
                 <span>
-                    <Badge variant="soft" color="teal" size="xs">Beta</Badge>
+                    <Badge variant="soft" color="orange" size="xs">Beta</Badge>
                 </span>
             </a>
         </div>
